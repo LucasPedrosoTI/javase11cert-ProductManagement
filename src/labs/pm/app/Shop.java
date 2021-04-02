f5c55b41d462d6d471c1283cc64a4dc2a5843d14
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2021-04-02 19:00:41 -0300

    :hammer: feat: Add enum for rating, update product, make product immutable Ref #5:Improved Class Design

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -3,15 +3,27 @@ package labs.pm.app;
 import java.math.BigDecimal;
 
 import labs.pm.app.data.Product;
+import labs.pm.app.data.Rating;
 
 public class Shop {
 
 	public static void main(String[] args) {
 		
-		Product p1 = new Product();
-		p1.setId(101);
-		p1.setName("Tea");
-		p1.setPrice(BigDecimal.valueOf(1.99));
+		Product p1 = new Product(101, "Tear", BigDecimal.valueOf(1.99));
+		Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STARS);
+		Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STARS);
+		Product p4 = new Product();
+		Product p5 = p3.applyRating(Rating.THREE_STARS);
+		
+//		p1.setId(101);
+//		p1.setName("Tea");
+//		p1.setPrice(BigDecimal.valueOf(1.99))
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+		System.out.println(p5);
 		
 	}
 
--- src/labs/pm/app/data/Product.java
+++ src/labs/pm/app/data/Product.java
@@ -1,36 +1,77 @@
 package labs.pm.app.data;
 
 import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.app.data.Rating.*;
 
 import java.math.BigDecimal;
 
 public class Product {
-	
-	private int id;
-	private String name;
-	private BigDecimal price;
-	public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);  
-	
+
+	public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+	private final int id; // immutable class
+	private final String name;
+	private final BigDecimal price;
+	private final Rating rating;
+
+	public Product(int id, String name, BigDecimal price, Rating rating) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.rating = rating;
+	}
+
+	public Product(int id, String name, BigDecimal price) {
+		this(id, name, price, NOT_RATED);
+	}
+
+	public Product(){
+		this(0, "no name", BigDecimal.ZERO);
+	}
+
 	public int getId() {
 		return id;
 	}
-	public void setId(final int id) {
-		this.id = id;
-	}
+
+	// public void setId(final int id) {
+	// 	this.id = id;
+	// }
+
 	public String getName() {
 		return name;
 	}
-	public void setName(final String name) {
-		this.name = name;
-	}
+
+	// public void setName(final String name) {
+	// 	this.name = name;
+	// }
+
 	public BigDecimal getPrice() {
 		return price;
 	}
-	public void setPrice(final BigDecimal price) {
-		this.price = price;
+
+	// public void setPrice(final BigDecimal price) {
+	// 	this.price = price;
+	// }
+
+	public Rating getRating() {
+		return rating;
 	}
-	
+
+	public Product applyRating(Rating newRating){
+		return new Product(id, name, price, newRating);
+	}
+
 	public BigDecimal getDiscount() {
 		return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
 	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [id=").append(id).append(", name=").append(name).append(", price=").append(price)
+				.append(", rating=").append(rating).append(", discount= ").append(this.getDiscount()).append("]");
+		return builder.toString();
+	}
+	
+	
+
 }
